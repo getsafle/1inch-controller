51b2030a040701a28657cbb9a1c7ce3a5d61636b
Suraj Singla <suraj.singla@mckinleyrice.co>
2022-01-25 19:44:59 +0530

    Added correct SUPPORTED_TOKENS_URL

--- src/config/index.js
+++ src/config/index.js
@@ -1,3 +1,4 @@
-module.exports ={
-    SUPPORTED_TOKENS_URL : 'https://gist.githubusercontent.com/sshubhamagg/2d07a88acecf02208d930d9b8598e96c/raw/207cf22609c0cf84a21d78c3cd8387ed654fda83/tokens.json',
+module.exports = {
+    SUPPORTED_TOKENS_URL: 'https://safle.api.enterprise.1inch.exchange/v4.0/1/tokens'
+
 }
