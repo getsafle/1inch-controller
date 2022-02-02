a91482468c52093a0bf64c313911f0e6b217cb3f
Suraj Singla <suraj.singla@mckinleyrice.co>
2022-02-02 13:37:47 +0530

    getEstimatedGas() added

--- src/index.js
+++ src/index.js
@@ -18,6 +18,14 @@ class OneInch {
         return response;
     }
 
+    async getEstimatedGas(toToken, fromToken, quantity) {
+        const URL = `${config.EXCHANGE_RATE_URL}?fromTokenAddress=${fromToken}&toTokenAddress=${toToken}&amount=${quantity}`
+        const { response, error } = await helper.getRequest({ url: URL });
+        if (error)
+            throw error
+        return { estimatedGas: response.estimatedGas };
+    }
+
     async getRawTransaction(walletAddress, toContractAddress, fromContractAddress, toQuantity, fromQuantity, slippageTolerance = 1) {
         const URL = `${config.SWAP_URL}?fromTokenAddress=${fromContractAddress}&toTokenAddress=${toContractAddress}&amount=${fromQuantity}&fromAddress=${walletAddress}&slippage=${slippageTolerance}`
         const { response, error } = await helper.getRequest({ url: URL });
