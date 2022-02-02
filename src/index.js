94e6c38b8d1fc9e7565b0d8fd3f07e216a895afb
Suraj Singla <suraj.singla@mckinleyrice.co>
2022-02-02 14:30:05 +0530

    Added checksum addresses fixes

--- src/index.js
+++ src/index.js
@@ -1,5 +1,6 @@
 const config = require('./config');
 const helper = require('./utils/helper')
+const web3Utils = require('web3-utils')
 
 class OneInch {
 
@@ -10,7 +11,9 @@ class OneInch {
         return response;
     }
 
-    async getExchangeRate(toToken, fromToken, quantity) {
+    async getExchangeRate(_toToken, _fromToken, quantity) {
+        const toToken = web3Utils.toChecksumAddress(_toToken)
+        const fromToken = web3Utils.toChecksumAddress(_fromToken)
         const URL = `${config.EXCHANGE_RATE_URL}?fromTokenAddress=${fromToken}&toTokenAddress=${toToken}&amount=${quantity}`
         const { response, error } = await helper.getRequest({ url: URL });
         if (error)
@@ -18,7 +21,9 @@ class OneInch {
         return response;
     }
 
-    async getEstimatedGas(toToken, fromToken, quantity) {
+    async getEstimatedGas(_toToken, _fromToken, quantity) {
+        const toToken = web3Utils.toChecksumAddress(_toToken)
+        const fromToken = web3Utils.toChecksumAddress(_fromToken)
         const URL = `${config.EXCHANGE_RATE_URL}?fromTokenAddress=${fromToken}&toTokenAddress=${toToken}&amount=${quantity}`
         const { response, error } = await helper.getRequest({ url: URL });
         if (error)
@@ -26,7 +31,10 @@ class OneInch {
         return { estimatedGas: response.estimatedGas };
     }
 
-    async getRawTransaction(walletAddress, toContractAddress, fromContractAddress, toQuantity, fromQuantity, slippageTolerance = 1) {
+    async getRawTransaction(_walletAddress, _toContractAddress, _fromContractAddress, toQuantity, fromQuantity, slippageTolerance = 1) {
+        const toContractAddress = web3Utils.toChecksumAddress(_toContractAddress)
+        const fromContractAddress = web3Utils.toChecksumAddress(_fromContractAddress)
+        const walletAddress = web3Utils.toChecksumAddress(_walletAddress)
         const URL = `${config.SWAP_URL}?fromTokenAddress=${fromContractAddress}&toTokenAddress=${toContractAddress}&amount=${fromQuantity}&fromAddress=${walletAddress}&slippage=${slippageTolerance}`
         const { response, error } = await helper.getRequest({ url: URL });
         if (error)
