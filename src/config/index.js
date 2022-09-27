f11ca48d83ea15d80477e478a2276b6ea05bf312
Suraj Singla <suraj.singla@mckinleyrice.co>
2022-09-27 16:30:23 +0530

    polygon RPC url update

--- src/config/index.js
+++ src/config/index.js
@@ -4,6 +4,6 @@ module.exports = {
     ETHEREUM_ADDRESS: '0xEeeeeEeeeEeEeeEeEeEeeEEEeeeeEeeeeeeeEEeE',
     SWAP_ROUTER_ADDRESS: '0x1111111254fb6c44bAC0beD2854e76F90643097d',
     ETH_RPC: 'https://mainnet.infura.io/v3/2851f8f32e8f4476bb4b9a7b9c6e5292',
-    POLYGON_RPC: 'https://polygon-rpc.com',
+    POLYGON_RPC: 'https://polygon-mainnet.infura.io/v3/2851f8f32e8f4476bb4b9a7b9c6e5292',
     BSC_RPC: 'https://bsc-dataseed1.binance.org/'
 }
